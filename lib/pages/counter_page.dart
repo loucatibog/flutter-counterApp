6ed31dc61f8c23a8c77960490ce8ef6ca39faed7
Lou Catibog <loucatibog30@gmail.com>
2025-09-28 14:04:40 +0100

    Setup files for future use

---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  // variables
+
+  // methods
+
+  // UI
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
